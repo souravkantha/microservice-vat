ac03a1fc978ba32a449743834eb09b4f358b1422
Sourav Kantha <souravkantha@Souravs-Air.Dlink>
2023-01-28 22:59:30 +0530

    initial commit

---
+++ src/test/java/com/mircoservice/vat/MicroserviceVatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mircoservice.vat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceVatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
